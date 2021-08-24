e4685abbee571803dd46264e03cc17df108162ab
litt1e-p <free9fw@gmail.com>
2021-08-24 09:59:54 +0800

    release v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # idbable
-The indexDB wrapper for browser which supports IE
+
+[![npm](https://img.shields.io/npm/v/idable?color=green)]((https://www.npmjs.com/package/idable))
+[![npm](https://img.shields.io/npm/dm/idable.svg)](https://www.npmjs.com/package/idable)
+
+The promisify indexDB wrapper for browser which supports IE
+
+## Installation
+
+```
+npm i idable
+
+import idable from 'idable'
+
+// or
+import { idable } from 'idable'
+
+const dao = new idable('dbName', 'tableName')
+
+```
+
+## Usage
+
+- `setItem`:
+
+```js
+await dao.setItem('key', 'value')
+```
+
+- `getItem`:
+
+```js
+await dao.getItem('key')
+```
+
+- `delItem`:
+
+```js
+await dao.delItem('key')
+```
+
+- `keys`:
+
+```js
+await dao.keys()
+```
+
+- `values`:
+
+```js
+await dao.values()
+```
+
+- `clear`:
+```js
+await dao.clear()
+```
+
+- `dropDb`:
+```js
+await dao.dropDb()
+```
--- src/core/dbs.js
+++ src/core/dbs.js
@@ -1,28 +1,41 @@
+/*
+ * Created on 2021-08-23 14:59:05
+ * Created by litt1e-p
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2020 litt1e-p
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 import { openDb, deleteDb } from './idb.polyfill.js'
 
-if (!('indexedDB' in window)) {
-  throw new Error('Fatal error: the browser does not support indexedDb');
-}
-
 class dbs {
-  constructor (tableName, dbName) {
-      var _db;
-      this.dbName = dbName
-      this.tableName = tableName
-      Object.defineProperty(this, 'db', {
-          async get () {
-            if (_db !== undefined) {
-              return _db
-            }
-            _db = await openDb(this.dbName, 1, db => {
-              db.createObjectStore(this.tableName)
-            })
-            return _db
-          },
-          set () { 
-            throw new ReferenceError('Fatal error: db is not settable')
-          }
-      });
+  constructor (dbName, tableName) {
+    if (!('indexedDB' in window)) {
+      throw new Error('Fatal error: the browser does not support indexedDb')
+    }
+    this.dbName = dbName
+    this.tableName = tableName
+    Object.defineProperty(this, 'db', {
+      async get () {
+        const _db = await openDb(this.dbName, 1, db => {
+          db.createObjectStore(this.tableName)
+        })
+        return _db
+      },
+      set () { 
+        throw new ReferenceError('Fatal error: db is not settable')
+      }
+    })
   }
 
   async setItem (key, value) {
@@ -44,7 +57,7 @@ class dbs {
     return tx.complete;
   }
 
-  async lear () {
+  async clear () {
     const db = await this.db;
     const tx = db.transaction(this.tableName, 'readwrite');
     tx.objectStore(this.tableName).clear();
